a213cd9c774d7dc62cd035a62ef6aa82d97aac2a
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2022-10-17 16:56:05 -0300

    feat :  Adicionando menu lateral na aplicação.

---
+++ src/app/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,43 @@
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material";
+import { Box } from '@mui/system';
+import { House } from "phosphor-react";
+import SidImg from '../../assets/sid.jpg'
+
+interface IMenuLateralProps {
+    children: React.ReactNode
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+
+    const theme = useTheme();
+
+    return (
+        <>
+            <Drawer variant="permanent">
+                <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+                    <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+                        <Avatar
+                            sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+                            src={SidImg}
+                        />
+                    </Box>
+                    <Divider />
+                    <Box flex={1}>
+                        <List component="nav">
+                            <ListItemButton alignItems="center" >
+                                <ListItemIcon>
+                                    <House weight="fill" />
+                                </ListItemIcon>
+                                <ListItemText primary="Página inicial" />
+                            </ListItemButton>
+                        </List>
+                    </Box>
+                </Box>
+            </Drawer>
+
+            <Box height="100vh" marginLeft={theme.spacing(28)}>
+                {children}
+            </Box>
+        </>
+    );
+}
