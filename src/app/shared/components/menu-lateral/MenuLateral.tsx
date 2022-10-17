38e627f249e06e9f773a02f701594927b2caa11c
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2022-10-17 17:33:15 -0300

    feact : Adicionando imagem de perfil.

--- src/app/shared/components/menu-lateral/MenuLateral.tsx
+++ src/app/shared/components/menu-lateral/MenuLateral.tsx
@@ -18,8 +18,7 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
                     <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
                         <Avatar
                             sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
-                            src={SidImg}
-                        />
+                            src={SidImg}/>
                     </Box>
                     <Divider />
                     <Box flex={1}>
