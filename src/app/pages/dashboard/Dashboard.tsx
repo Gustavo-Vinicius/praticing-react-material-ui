9a3037e06611a1df48d656f4eb1c7dd940783e36
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2022-10-18 17:54:29 -0300

    feat :  Adicionando barra de ferramentas na aplicação.

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,14 @@
+import { BarraDeFerramentas } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 
 export const Dashboard = () => {
     return (
-        <LayoutBaseDePagina titulo="Página inicial" barraDeFerramentas ={<>Barra de Ferramentas</>}>
+        <LayoutBaseDePagina
+            titulo="Página inicial" barraDeFerramentas={(
+                <BarraDeFerramentas
+                    mostrarInputBusca
+                />
+            )}>
             Testendo
         </LayoutBaseDePagina>
     );
---
+++ src/app/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -0,0 +1,58 @@
+import { Button, Paper, TextField, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+import { Plus } from "phosphor-react";
+
+interface IBarraDeFerramentasProps {
+    textoDaBusca?: string;
+    mostrarInputBusca?: boolean;
+    aoMudarTextDeBusca?: (novoTexto: string) => void;
+    textoBotaoNovo?: string;
+    mostrarBotaoNovo?: boolean;
+    aoCLicarEmNovo?: () => void;
+}
+
+export const BarraDeFerramentas: React.FC<IBarraDeFerramentasProps> = ({
+    textoDaBusca = '',
+    mostrarInputBusca = false,
+    aoMudarTextDeBusca,
+    aoCLicarEmNovo,
+    textoBotaoNovo = 'Novo',
+    mostrarBotaoNovo = true
+}) => {
+
+    const theme = useTheme();
+
+    return (
+        <Box
+            gap={1}
+            marginX={1}
+            padding={1}
+            paddingX={2}
+            display='flex'
+            alignItems='center'
+            height={theme.spacing(5)}
+            component={Paper}
+        >
+            {mostrarInputBusca && (
+                <TextField
+                    size='small'
+                    value={textoDaBusca}
+                    placeholder='Pesquisar...'
+                    onChange={(e) => aoMudarTextDeBusca?.(e.target.value)}
+
+                />
+            )}
+            <Box flex={1} display='flex' justifyContent='end'>
+                {mostrarBotaoNovo && (
+                    <Button
+                        color='primary'
+                        disableElevation
+                        variant='contained'
+                        onClick={aoCLicarEmNovo}
+                        endIcon={<Plus />}>
+                        {textoBotaoNovo}</Button>
+                )}
+            </Box>
+        </Box>
+    );
+}
