c6424c2a84fe240d2e3dfd0c18a0644245e8b73d
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2022-10-20 10:59:14 -0300

    refactor :  Refatorando pasta de configuração de axios.

--- src/app/shared/services/api/axios-config/index.ts
+++ src/app/shared/services/api/axios-config/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Enviroment } from '../../../environment';
-import { erroInterceptor, responseInterceptor } from '../interceptors';
+import { erroInterceptor, responseInterceptor } from './interceptors';
 
 const Api = axios.create({
     baseURL: Enviroment.URL_BASE
